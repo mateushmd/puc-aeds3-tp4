6238796eca4307cb89f0ced33144791a9070d194
mateushmd <mateushmdiniz@gmail.com>
2025-06-04 21:36:21 -0300

    visualização dos diretórios

--- script.js
+++ script.js
@@ -1,100 +1,96 @@
-const tCesto = int(document.querySelector('#cesto').value);
-const tTotal = tCesto * 4; // 4 bytes para armazenar um inteiro
+const tItem = 4;
+let tCesto = 4;
+let tTotal = tCesto * tItem;
 
-class Cesto 
-{
-    constructor(p, endereco) 
-    {
+const cestosEl = document.querySelector('#diretorio');
+const pEl = document.querySelector('#profundidade-global');
+
+document.querySelector('#tamanho-cesto').addEventListener('change', (e) => {
+    tCesto = e.target.value;
+    tTotal = tCesto * tItem;
+});
+
+class Cesto {
+    constructor(p, endereco, cestoEl) {
         this.p = p;
         this.endereco = endereco;
         this.quantidade = 0;
         this.elementos = [];
+        this.cestoEl = cestoEl;
     }
 
-    inserir(valor)
-    { 
-        if(this.quantidade === tCesto)
+    inserir(valor) {
+        if (this.quantidade === tCesto)
             return false;
 
-        let i = quantidade - 1;
-        while(i >= 0 && this.elementos[i] > valor) i--;
+        let i = this.quantidade - 1;
+        while (i >= 0 && this.elementos[i] > valor) i--;
         this.elementos.splice(i + 1, 0, valor);
         this.quantidade++;
         return true;
     }
 
-    remover(valor)
-    {
-        if(this.quantidade === 0)
-            return false;
-
-        for(let i = 0; i < this.quantidade; i++)
-        {
-            if(this.elementos[i] === valor)
-            {
-                this.elementos.splice(i, 1);
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    buscar(valor)
-    {
+    buscar(valor) {
         return this.#buscar(valor, 0, this.quantidade - 1);
     }
 
-    #buscar(valor, esq, dir)
-    {
+    #buscar(valor, esq, dir) {
         let meio = Math.floor((esq + dir) / 2);
-        if(esq > dir) return false;
-        if(this.elementos[meio] == valor) return true;
-        return this.#buscar(valor, esq, meio - 1) || this.#buscar(valor, meio + 1, dir); 
+        if (esq > dir) return false;
+        if (this.elementos[meio] == valor) return true;
+        return this.#buscar(valor, esq, meio - 1) || this.#buscar(valor, meio + 1, dir);
     }
 
-    limpar()
-    {
-        elementos = [];
-        quantidade = 0;
+    limpar() {
+        this.elementos = [];
+        this.quantidade = 0;
     }
 }
 
 let diretorio = [0];
+let dElements = [];
 let cestos = [new Cesto(0, 0)];
 let p = 0;
 
-function hash(valor)
-{
+dElements.push(criarElementoDiretorio(0));
+
+popular();
+
+function hash(valor) {
     return valor % (2 ** p);
 }
 
-function inserir(valor)
-{
+function inserir(valor) {
     let idx = hash(valor);
     let cesto = cestos[diretorio[idx]];
 
-    if(cesto.inserir(valor))
+    if (cesto.inserir(valor))
         return;
 
     cesto.p++;
-    let novoCesto = new Cesto(cesto.p, tTotal * (cestos.length)); 
+    let novoCesto = new Cesto(cesto.p, tTotal * (cestos.length));
     cestos.push(novoCesto);
 
-    if(cesto.p <= p)
-    {
-        for(let i = diretorio.length - 1; i >= 0; i--)
-        {
-            if(diretorio[i] === diretorio[idx])
+    if (cesto.p <= p) {
+        for (let i = diretorio.length - 1; i >= 0; i--) {
+            if (diretorio[i] === diretorio[idx]) {
                 diretorio[i] = cestos.length - 1;
+                dElements[i].innerHTML = cestos.length - 1;
+            }
         }
     }
-    else
-    {
+    else {
         let copiaDiretorio = [...diretorio];
         copiaDiretorio[idx] = cestos.length - 1;
-        diretorio.push(copiaDiretorio);
+        copiaDiretorio.forEach(el => diretorio.push(el));
+
+        let copiaDElements = [];
+        dElements.forEach(el => copiaDElements.push(criarElementoDiretorio(el.innerHTML)));
+        copiaDElements[idx].innerHTML = cestos.length - 1;
+        copiaDElements.forEach(el => dElements.push(el));
+
         p++;
+        pEl.innerHTML = p;
     }
 
     let elementos = [...cesto.elementos];
@@ -104,8 +100,39 @@ function inserir(valor)
     elementos.forEach(e => inserir(e));
 }
 
-function remover(valor)
-{
+function buscar(valor) {
     let idx = hash(valor);
-    cestos[diretorio[idx]].remover(valor);
+    return cestos[diretorio[idx]].buscar(valor);
+}
+
+function reiniciar() {
+    diretorio = [0];
+    cestos = [new Cesto(0, 0)];
+    p = 0;
+}
+
+function popular() {
+    for (let i = 1; i <= 10; i++) inserir(i);
+}
+
+function criarElementoDiretorio(valor) {
+    const el = document.createElement('tr');
+    el.innerHTML = valor;
+    cestosEl.appendChild(el);
+    return el;
+}
+
+function criarElementoCesto(endereco) {
+    const el = document.createElement('tr');
+    const colunaEnderecoEl = document.createElement('td', { class: 'endereco' });
+    colunaEnderecoEl.innerHTML = endereco;
+    el.appendChild(colunaEnderecoEl);
+    el.appendChild(document.createElement('td', { class: 'profundidade-local' }));
+    el.appendChild(document.createElement('td', { class: 'qtd-itens' }));
+
+    // Um <td> para cada item (dinâmico)
+    for (let i = 0; i < tCesto; i++)
+        el.appendChild(document.createElement('td', { class: 'item-cesto' }));
+
+    return el;
 }
